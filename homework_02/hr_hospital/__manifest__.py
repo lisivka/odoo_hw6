be1853a6e2332850eb7db6001f1d0bfe292fac45
lisivka <lisivka@gmail.com>
2024-09-15 15:29:03 +0300

    fix data, add description

--- homework_02/hr_hospital/__manifest__.py
+++ homework_02/hr_hospital/__manifest__.py
@@ -1,10 +1,18 @@
 {
     'name': 'HR Hospital',
     'author': 'Lisivka Sergii',
-    'summary': '',
+    'category': 'Human Resources',
+    'summary': 'Manage Hospital Operations',
+    'description': """
+        Module to manage hospital operations:
+        - Doctors
+        - Patients
+        - Diseases
+        - Visits
+    """,
     'website': 'https://odoo.school/',
     'category': 'Customizations',
-    'version': '17.0.0.0.0',
+    'version': '17.0.0.0.2',
     'license': 'OPL-1',
     'depends': [
         'base',
@@ -16,13 +24,16 @@
 
     'data': [
         'security/ir.model.access.csv',
-        'views/hr_hospital_menu.xml',
-        'views/disease_views.xml',
         'views/doctor_views.xml',
         'views/patient_views.xml',
+        'views/disease_views.xml',
         'views/visit_views.xml',
+        'views/hr_hospital_menu.xml',  # Завантажується останнім
     ],
-    'demo': [],
+
+    'demo': [
+         'demo/hr_hospital_demo.xml',
+    ],
 
     'installable': True,
     'auto_install': False,
