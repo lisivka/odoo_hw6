36e2fe2e64949bab2e1d94e091abb7fecae84e4e
lisivka <lisivka@gmail.com>
2024-09-18 23:35:47 +0300

    add personal info to model patient

--- homework_03/hr_hospital/models/hr_hospital_patient.py
+++ homework_03/hr_hospital/models/hr_hospital_patient.py
@@ -1,6 +1,7 @@
 import logging
 
-from odoo import models, fields
+from datetime import date
+from odoo import models, fields, api
 
 _logger = logging.getLogger(__name__)
 
@@ -10,5 +11,22 @@ class Patient(models.Model):
     _name = 'hr.hospital.patient'
     _description = 'Patient'
 
+    age = fields.Integer(string='Age', compute='_compute_age', store=True)
+    birth_date = fields.Date(string='Birth Date')
+    personal_doctor_id = fields.Many2one('hr.hospital.doctor',
+                                         string='Personal Doctor')
+    passport_details = fields.Char(string='Passport Details')
+    contact_person = fields.Char(string='Contact Person')
     visit_ids = fields.One2many('hr.hospital.visit', 'patient_id',
                                 string='Visits')
+
+    @api.depends('birth_date')
+    def _compute_age(self):
+        for record in self:
+            if record.birth_date:
+                today = date.today()
+                record.age = today.year - record.birth_date.year - (
+                            (today.month, today.day) < (
+                    record.birth_date.month, record.birth_date.day))
+            else:
+                record.age = 0
