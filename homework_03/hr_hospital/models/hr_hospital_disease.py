d2b49d2edcb9961d2f71d217f4c4cd2f34605955
lisivka <lisivka@gmail.com>
2024-09-29 12:10:38 +0300

    add kanban and pivot

--- homework_03/hr_hospital/models/hr_hospital_disease.py
+++ homework_03/hr_hospital/models/hr_hospital_disease.py
@@ -8,9 +8,18 @@ _logger = logging.getLogger(__name__)
 class Disease(models.Model):
     _name = 'hr.hospital.disease'
     _description = 'Disease'
+    _parent_name = 'parent_id'
+    _parent_store = True
+    _child_order = 'sequence, name'
 
     name = fields.Char(string='Disease Name', required=True)
     description = fields.Text(string='Description')
-    parent_id = fields.Many2one('hr.hospital.disease', string='Parent Disease')
-    child_ids = fields.One2many('hr.hospital.disease', 'parent_id', string='Sub-Diseases')
+    parent_id = fields.Many2one('hr.hospital.disease',
+                                string='Parent Disease',
+                                ondelete='cascade',
+                                index=True)
+    child_ids = fields.One2many('hr.hospital.disease',
+                                'parent_id',
+                                string='Sub-Diseases')
     sequence = fields.Integer(default=10)
+    parent_path = fields.Char(index=True)
