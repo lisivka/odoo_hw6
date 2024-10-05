a1fe7027161a60ef76479f08d4d4353fb9f76db7
lisivka <lisivka@gmail.com>
2024-10-05 21:16:09 +0300

    add inline for doctor, patient

--- homework_04/hr_hospital/models/hr_hospital_diagnosis.py
+++ homework_04/hr_hospital/models/hr_hospital_diagnosis.py
@@ -1,6 +1,7 @@
 import logging
 
 from odoo import models, fields, api
+from odoo.tools.populate import compute
 
 _logger = logging.getLogger(__name__)
 
@@ -12,39 +13,70 @@ class Diagnosis(models.Model):
 
     name = fields.Char(compute='_compute_name',
                        store=True)
-    visit_id = fields.Many2one('hr.hospital.visit',
-                               ondelete='cascade')
+    is_approved = fields.Boolean(tracking=True)
+    doctor_id = fields.Many2one('hr.hospital.doctor')
+    # doctor_name = fields.Char(compute='_compute_person_names',
+    #                           store=True)
     disease_id = fields.Many2one('hr.hospital.disease',
                                  required=True)
     description = fields.Text(tracking=True)
-    approved = fields.Boolean(tracking=True)
+    # patient_name = fields.Char(compute='_compute_person_names',
+    #                            store=True)
 
-    patient_name = fields.Char(compute='_compute_person_names',
-                               store=True)
-    doctor_name = fields.Char(compute='_compute_person_names',
-                              store=True)
-    # diagnosis_details = fields.Text()
+    patient_id = fields.Many2one('hr.hospital.patient',
+                                 compute = '_compute_patient_id',
+                                 ondelete='cascade')
+    visit_id = fields.Many2one('hr.hospital.visit',
+                               ondelete='cascade')
 
-    @api.depends('patient_name', 'doctor_name')
+    mentor_id = fields.Many2one('hr.hospital.doctor', compute='_compute_doctor_info', store=True)
+    can_approve = fields.Boolean(compute='_compute_can_approve')
+
+    @api.depends('visit_id.patient_id')
+    def _compute_patient_id(self):
+        for rec in self:
+            if rec.visit_id and rec.visit_id.patient_id:
+                rec.patient_id = rec.visit_id.patient_id
+            else:
+                rec.patient_id = False
+
+    @api.depends('visit_id')
+    def _compute_doctor_info(self):
+        for rec in self:
+            if rec.visit_id:
+                doctor = rec.visit_id.doctor_id
+                rec.doctor_id = doctor
+                rec.mentor_id = doctor.mentor_id if doctor.is_intern else False
+
+    @api.depends('doctor_id')
+    def _compute_can_approve(self):
+        for rec in self:
+            # Можна погодити лише якщо лікар є інтерном
+            rec.can_approve = rec.doctor_id.is_intern
+
+    @api.depends('patient_id', 'doctor_id')
     def _compute_name(self):
         """Обчислює значення для збереженого поля name"""
         for record in self:
-            record.name = (f"{record.patient_name}" +
-                           f" | {record.doctor_name}" +
+            record.name = (f"{record.patient_id.name}" +
+                           f" | {record.doctor_id.name}" +
                            f" | {record.disease_id.name}" +
-                           f" | {record.description}")
+                           f" | ")
 
-    @api.depends('visit_id')
-    def _compute_person_names(self):
-        """Обчислює ім'я та прізвище пацієнта і лікаря"""
-        for rec in self:
-            if rec.visit_id:
-                # Отримуємо дані пацієнта
-                patient = rec.visit_id.patient_id
-                rec.patient_name = (f"{patient.first_name} {patient.last_name}"
-                                    if patient else "")
 
-                # Отримуємо дані лікаря
-                doctor = rec.visit_id.doctor_id
-                rec.doctor_name = (f"{doctor.first_name} {doctor.last_name}"
-                                   if doctor else "")
+
+    @api.model
+    def create(self, vals):
+        if 'visit_id' in vals and vals['visit_id']:
+            visit = self.env['hr.hospital.visit'].browse(vals['visit_id'])
+            if visit.doctor_id:
+                vals['doctor_id'] = visit.doctor_id.id
+        return super(Diagnosis, self).create(vals)
+
+    @api.onchange('visit_id')
+    def _onchange_visit_id(self):
+        if self.visit_id:
+            self.doctor_id = self.visit_id.doctor_id
+            self.patient_id = self.visit_id.patient_id
+
+
