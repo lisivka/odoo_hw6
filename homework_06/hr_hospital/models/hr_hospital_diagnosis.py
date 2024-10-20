4e32d8da94a80c2c554e2d72ed89377e18692040
lisivka <lisivka@gmail.com>
2024-10-20 12:24:36 +0000

    add docstring

--- homework_06/hr_hospital/models/hr_hospital_diagnosis.py
+++ homework_06/hr_hospital/models/hr_hospital_diagnosis.py
@@ -5,6 +5,15 @@ _logger = logging.getLogger(__name__)
 
 
 class Diagnosis(models.Model):
+    """
+    Model representing a medical diagnosis.
+
+    This model stores information about a medical diagnosis, including
+    the associated doctor,patient, disease, and other related fields.
+    It inherits from `mail.thread` to allow tracking of field changes
+    in the diagnosis.
+    """
+
     _name = 'hr.hospital.diagnosis'
     _inherit = ['mail.thread']
     _description = 'Diagnosis'
@@ -36,6 +45,14 @@ class Diagnosis(models.Model):
 
     @api.depends('visit_id.patient_id')
     def _compute_patient_id(self):
+        """
+        Compute the patient associated with the diagnosis based on the visit.
+
+        If there is a related visit with an associated patient,
+        this method assigns the  patient to the `patient_id` field.
+        Otherwise, it sets the field to False.
+        """
+
         for rec in self:
             if rec.visit_id and rec.visit_id.patient_id:
                 rec.patient_id = rec.visit_id.patient_id
@@ -44,6 +61,14 @@ class Diagnosis(models.Model):
 
     @api.depends('visit_id')
     def _compute_doctor_info(self):
+        """
+        Compute the doctor and mentor associated with the diagnosis.
+
+        If the diagnosis is linked to a visit,
+        this method assigns the doctor from the  visit and,
+        if applicable, assigns the mentor if the doctor is an intern.
+        """
+
         for rec in self:
             if rec.visit_id:
                 doctor = rec.visit_id.doctor_id
@@ -52,13 +77,26 @@ class Diagnosis(models.Model):
 
     @api.depends('doctor_id')
     def _compute_can_approve(self):
+        """
+        Determine whether the diagnosis can be approved.
+
+        This method checks if the assigned doctor is an intern.
+        Only an intern can approve the diagnosis.
+        """
+
         for rec in self:
             # Можна погодити лише якщо лікар є інтерном
             rec.can_approve = rec.doctor_id.is_intern
 
     @api.depends('disease_id')
     def _compute_disease_type(self):
-        """Обчислюємо тип хвороби на основі parent_id (якщо існує)"""
+        """
+        Compute the disease type based on the selected disease.
+
+        If the selected disease has a parent disease
+        (i.e., it is a sub-disease), this method
+        assigns the parent disease to the `disease_type_id` field.
+        """
         for record in self:
             if record.disease_id:
                 record.disease_type_id = record.disease_id.parent_id
@@ -67,7 +105,7 @@ class Diagnosis(models.Model):
 
     @api.depends('patient_id', 'doctor_id')
     def _compute_name(self):
-        """Обчислює значення для збереженого поля name"""
+        """Compute the name field of the diagnosis."""
         for record in self:
             record.name = (f"{record.patient_id.name}" +
                            f" | {record.doctor_id.name}" +
@@ -75,6 +113,7 @@ class Diagnosis(models.Model):
 
     @api.model
     def create(self, vals):
+        """ Override the create method to assign the doctor from the visit. """
         if 'visit_id' in vals and vals['visit_id']:
             visit = self.env['hr.hospital.visit'].browse(vals['visit_id'])
             if visit.doctor_id:
@@ -83,6 +122,7 @@ class Diagnosis(models.Model):
 
     @api.onchange('visit_id')
     def _onchange_visit_id(self):
+        """ Update the doctor and patient when the visit is changed. """
         if self.visit_id:
             self.doctor_id = self.visit_id.doctor_id
             self.patient_id = self.visit_id.patient_id
--- homework_06/hr_hospital/models/hr_hospital_disease.py
+++ homework_06/hr_hospital/models/hr_hospital_disease.py
@@ -6,6 +6,13 @@ _logger = logging.getLogger(__name__)
 
 
 class Disease(models.Model):
+    """
+    Model representing a disease.
+
+    This model stores information about diseases, including their names, descriptions,
+    and any parent-child relationships for sub-diseases.
+    """
+
     _name = 'hr.hospital.disease'
     _description = 'Disease'
     _parent_name = 'parent_id'
